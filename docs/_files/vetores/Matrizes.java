ae2cb6fd875f168dd5bc0f76add19ea5b8cf50bc
Darlon <darlonv@gmail.com>
2023-08-14 17:41:05 -0300

    content added

--- docs/_files/vetores/Matrizes.java
+++ docs/_files/vetores/Matrizes.java
@@ -15,5 +15,40 @@ public class Matrizes {
             System.out.println("");
         }
 
+        int[][] teste = null;
+
+        int l = 3;
+        int c = 3;
+        int counter = 0;
+
+        // teste = new int[l][c];
+
+        if (teste == null) {
+            System.out.println("Matriz não alocada");
+        } else {
+            System.out.println("Matriz alocada");
+        }
+
+        teste = new int[l][c];
+
+        if (teste == null) {
+            System.out.println("Matriz não alocada");
+        } else {
+            System.out.println("Matriz alocada");
+        }
+
+        for (i = 0; i < l; i++) {
+            for (j = 0; j < c; j++) {
+                teste[i][j] = ++counter;
+            }
+        }
+
+        for (i = 0; i < l; i++) {
+            for (j = 0; j < c; j++) {
+                System.out.printf("%d ", teste[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 }
