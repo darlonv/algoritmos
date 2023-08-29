f489d4145678d84c2da9f9393240192e9b492389
Darlon Vasata <darlonv@gmail.com>
2023-08-29 20:17:27 -0300

    content added

--- docs/_files/registros/Registro.java
+++ docs/_files/registros/Registro.java
@@ -7,28 +7,32 @@ class Produto    {
 }
 
 public class Registro {
-    
+
     public static String registroProdutoParaString(Produto prod){
         return String.format("%d:%f:%s", prod.lote, prod.peso, prod.descricao);
     }
 
     public static void registro() {
-        Produto prod_1 = new Produto();
-        Produto prod_2 = new Produto();
-        Produto prod_3 = new Produto();
-        prod_1.descricao = "abc";
-        prod_1.lote = 1;
-        prod_1.peso = 2.0f;
+        Produto prod = new Produto();
+        prod.lote = 10;
 
-        System.out.println(prod_1.descricao);
-        System.out.println(registroProdutoParaString(prod_1));
+        System.out.println(prod.lote);
+        // Produto prod_1 = new Produto();
+        // Produto prod_2 = new Produto();
+        // Produto prod_3 = new Produto();
+        // prod_1.descricao = "abc";
+        // prod_1.lote = 1;
+        // prod_1.peso = 2.0f;
 
-        prod_2.descricao = "def";
-        prod_2.lote = 1;
-        prod_2.peso = 2.0f;
+        // System.out.println(prod_1.descricao);
+        // System.out.println(registroProdutoParaString(prod_1));
 
-        System.out.println(prod_2.descricao);
-        System.out.println(registroProdutoParaString(prod_2));
+        // prod_2.descricao = "def";
+        // prod_2.lote = 1;
+        // prod_2.peso = 2.0f;
+
+        // System.out.println(prod_2.descricao);
+        // System.out.println(registroProdutoParaString(prod_2));
 
     }
 
